927ff572a5ec02a31eb6894f713940ec512caa5a
Baeyezin <bam1991@naver.com>
2022-11-11 09:49:19 +0900

    4rd

---
+++ test1108/test.js
@@ -0,0 +1,11 @@
+
+    // spon box hover start
+
+    $(document).ready(function(){
+        $('.spon_btm_box').hover(function(){
+            $(this).css({'background-color':'#d94d1a'});
+        },function(){
+                $(this).css('background-color','#007932'); 
+            });
+        });
+    
